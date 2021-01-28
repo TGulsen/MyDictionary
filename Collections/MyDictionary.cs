77c73fdf69aafcf404abc246faf28447992d7772
Tesnim Gülşen <kuzen_teso646@hotmail.com>
2021-01-28 23:59:59 +0300

    Add project files.

---
+++ Collections/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+
+        K[] temp_keys;
+        V[] temp_values;
+
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+
+        public void Add(K Key, V Value)
+        {
+            temp_keys = keys;
+            temp_values = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < temp_keys.Length; i++)
+            {
+                keys[i] = temp_keys[i];
+
+            }
+
+            for (int i = 0; i < temp_values.Length; i++)
+            {
+                values[i] = temp_values[i];
+            }
+
+
+
+            Console.WriteLine(" Anahtarların sayısı: " + keys.Length + " Değerin sayısı: " + values.Length  );
+            Console.WriteLine("Plaka numarası "+ Key + " olan " + Value + " şehri sisteme eklendi!");
+
+        }
+
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> sehirler = new Dictionary<int, string>();
+
+            sehirler.Add(1, "Adana");
+            sehirler.Add(2, "Adıyaman");
+            sehirler.Add(3, "Afyon");
+            sehirler.Add(4, "Ağrı");
+            sehirler.Add(5, "Amasya");
+            sehirler.Add(6, "Ankara");
+
+            foreach (var sehir in sehirler)
+            {
+                //Console.WriteLine(sehir.Key + " plakalı şehir: " + sehir.Value);
+            }
+
+
+            MyDictionary<int, string> x = new MyDictionary<int, string>();
+
+            x.Add(7, "Antalya");
+            x.Add(8, "Artvin");
+        }
+    }
+}
